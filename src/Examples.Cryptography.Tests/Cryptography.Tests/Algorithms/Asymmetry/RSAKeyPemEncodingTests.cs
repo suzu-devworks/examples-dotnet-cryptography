8f196e3228eea64b2afda06a1b374b07424f3f5f
A.suzuki <suzu.devworks@gmail.com>
2026-02-02 14:39:45 +0000

    fix: Update assertions in RSAKeyPemEncodingTests for accuracy and improved logging

--- src/Examples.Cryptography.Tests/Cryptography.Tests/Algorithms/Asymmetry/RSAKeyPemEncodingTests.cs
+++ src/Examples.Cryptography.Tests/Cryptography.Tests/Algorithms/Asymmetry/RSAKeyPemEncodingTests.cs
@@ -39,26 +39,27 @@ public class RSAKeyPemEncodingTests(
         output.WriteLine("Label: {0}", fields.Label);
         output.WriteLine("Base64Data: {0}", fields.Base64Data);
 
+        output.WriteLine("{0}", pem[fields.Label]);
+        output.WriteLine("{0}", pem[fields.Base64Data]);
+
         // Assert:
 
         // Gets the size of the decoded base-64 data, in bytes.
-        Assert.True(fields.DecodedDataLength is >= 2348 and <= 2400);
+        Assert.True(fields.DecodedDataLength is >= 2300 and <= 2400);
 
         // Gets the location of the PEM-encoded text,
         // including the surrounding encapsulation boundaries.
         Assert.Equal(0, fields.Location.Start);
-        Assert.Equal(3242, fields.Location.End);
+        Assert.True(fields.Location.End.Value is >= 3200 and <= 3300);
 
         // Gets the location of the label.
         Assert.Equal(11, fields.Label.Start);
         Assert.Equal(26, fields.Label.End);
         Assert.Equal("RSA PRIVATE KEY", pem[fields.Label]);
-        output.WriteLine("Label: {0}", pem[fields.Label]);
 
         // Gets the location of the base-64 data inside of the PEM.
         Assert.Equal(32, fields.Base64Data.Start);
-        Assert.Equal(3212, fields.Base64Data.End);
-        output.WriteLine("Base64Data: {0}", pem[fields.Base64Data]);
+        Assert.True(fields.Base64Data.End.Value is >= 3200 and <= 3300);
 
         var data = Convert.FromBase64String(pem[fields.Base64Data]);
 
